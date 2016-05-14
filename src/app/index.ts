fdb35b954468910e7f3cacacd41d714c53fce247
angular-cli <angular-cli@angular.io>
2016-05-14 06:37:52 +0530

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export {environment} from './environment';
+export {Ng2useAppComponent} from './ng2use.component';
---
+++ src/app/ng2use.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'ng2use-app',
+  templateUrl: 'ng2use.component.html',
+  styleUrls: ['ng2use.component.css']
+})
+export class Ng2useAppComponent {
+  title = 'ng2use works!';
+}
