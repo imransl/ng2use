2b4fdfc0baa3c1807d8a9dd853b8b6c00f62c7d2
Ahamed Imran <immy123@gmail.com>
2016-05-15 22:43:26 +0530

    added service call for block details

--- src/app/blocks/shared/block.service.ts
+++ src/app/blocks/shared/block.service.ts
@@ -10,11 +10,26 @@ export class BlockService {
   constructor(private http: Http) { }
 
   private blocksUrl = 'app/blocks/shared/block-data.json';
+  private githubUrl = '//api.github.com/repos/';
 
   getBlocks(): Observable<Block[]> {
     return this.http.get(this.blocksUrl)
       .map((res: Response) => res.json())
       .catch(this.handleError);
+  }
+  
+  getBlockDetails(repo): Observable<JSON> {
+    const url = this.githubUrl + repo;
+    return this.http.get(url)
+      .map(this.processData)
+      .cache()
+      .catch(this.handleError);
+  }
+  
+  processData(res: Response) {
+    let json = res.json();
+    console.log(json);
+    return json;
   }
 
   handleError(error: any) {
